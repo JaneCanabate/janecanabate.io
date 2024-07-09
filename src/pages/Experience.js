0b004604a1886fe8c41ecb3e9cbe51100f6fae90
Heart Marianne Rubio <hrtrubio@gmail.com>
2024-07-09 22:26:39 +0800

    update

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -18,8 +18,20 @@ function Experience() {
                         <Card className="w-50 border-end-2 radius-0 bg-transparent"></Card>
                         <Card className="w-lg-50 border-0 shadow m-auto p-2 p-lg-3 text-justify position-relative">
                         <p className="position-absolute top-0 end-0 me-3 mt-1 fw-bold text-primary text-opacity-50">2024</p>
-                        <h3 className="fw-bold">title</h3>
-                        <Image src={experience} fluid className="max-xy" />
+                        <Card.Title className="text-center fw-bold">Indoor Plants</Card.Title>
+                            <Row lg={2} xs={1}>
+                                <Col className="radius-0 m-auto">
+                                    <div className="overflow-hidden p-2 p-lg-4 d-flex justify-content-center">
+                                        <Image src={experience} fluid className="zoom" />
+                                    </div>
+                                </Col>
+                                <Col className="radius-0 m-auto text-justify ps-lg-0 pe-lg-4">
+                                Indoor plants are ornamental plants that are grown indoors.
+                                They are also known as houseplants, pot plants, or potted plants.
+                                Indoor plants are mainly grown in residences and offices for decorative purposes.
+                                Most houseplants are derived from plants native to the tropics and near tropics.
+                                </Col>
+                            </Row>
                         </Card>
                         <Card className="w-25 m-auto border-x radius-0 bg-transparent"></Card>
                         <Row>
