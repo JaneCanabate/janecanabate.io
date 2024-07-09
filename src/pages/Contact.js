27cf92d538c9e91a1d0dc738fcce8e4430b6908c
JaneCanabate <jccanabate2001@gmail.com>
2024-07-09 23:19:57 +0800

    final

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,17 +12,17 @@ function Contact(){
                                     <Card.Title className="text-center fs-2 poppins-bold">Lets Connect!!</Card.Title>
                                     <Card.Title className="text-center py-lg-3">You can reach me via:</Card.Title>
                                     <div className='text-center '>
-                                        <a href='link' ><i class="fs-1 px-3 bi bi-github text-primary"></i></a>
-                                        <a href='link' ><i class="fs-1 px-3 bi bi-instagram text-primary"></i></a>
-                                        <a href='link' ><i class="fs-1 px-3 bi bi-google text-primary"></i></a>
-                                        <a href='link' ><i class="fs-1 px-3 bi bi-facebook text-primary"></i></a>
+                                        <a href='https://github.com/JaneCanabate' ><i class="fs-1 px-3 bi bi-github text-primary"></i></a>
+                                        <a href='https://www.instagram.com/ms.jcunahap?igsh=ZHBxcHQ1bzdxN2E4' ><i class="fs-1 px-3 bi bi-instagram text-primary"></i></a>
+                                        <a href='jccanabate2001@gmail.com' ><i class="fs-1 px-3 bi bi-google text-primary"></i></a>
+                                        <a href='https://www.facebook.com/jane.canabate.9' ><i class="fs-1 px-3 bi bi-facebook text-primary"></i></a>
                                     </div>
                                 </Col>
                                     <Card.Title className="text-center">or</Card.Title>
                                 <Col className=" border-top border-primary border-2">
                                     <Card.Title className="text-center py-lg-3 pt-lg-5 poppins-bold-italic">Call Me</Card.Title>
                                     <div className="text-center">
-                                    <i class="fs-4 px-3 bi bi-telephone text-primary"> +6300000000</i>
+                                    <i class="fs-4 px-3 bi bi-telephone text-primary"> +639398304867</i>
                                     </div>
                                 </Col>
                             </Row>
