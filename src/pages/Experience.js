81f272938b766fa6e3c559d80172b1bbe69536d8
JaneCanabate <jccanabate2001@gmail.com>
2024-07-09 23:07:42 +0800

    new update

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,5 +1,8 @@
 import { Card, Col, Container, Row, Image } from "react-bootstrap";
-import experience from '../images/profile.jpg';
+import experience from '../images/christmas party.png';
+import experience1 from '../images/plaza type.jpg';
+import experience2 from '../images/shorting.jpg';
+import experience3 from '../images/last.png';
 
 function Experience() {
     return(
@@ -9,50 +12,68 @@ function Experience() {
                     <Col>
                         <Card className="w-lg-50 border-0 shadow m-auto p-2 p-lg-3 text-justify position-relative">
                         <p className="position-absolute top-0 end-0 me-3 mt-1 fw-bold text-primary text-opacity-50">2024</p>
-                        <h3 className="fw-bold">title</h3>
-                        Indoor plants are ornamental plants that are grown indoors.
-                                They are also known as houseplants, pot plants, or potted plants.
-                                Indoor plants are mainly grown in residences and offices for decorative purposes.
-                                Most houseplants are derived from plants native to the tropics and near tropics.
-                        </Card>
-                        <Card className="w-50 border-end-2 radius-0 bg-transparent"></Card>
-                        <Card className="w-lg-50 border-0 shadow m-auto p-2 p-lg-3 text-justify position-relative">
-                        <p className="position-absolute top-0 end-0 me-3 mt-1 fw-bold text-primary text-opacity-50">2024</p>
-                        <Card.Title className="text-center fw-bold">Indoor Plants</Card.Title>
-                            <Row lg={2} xs={1}>
+                        <h3 className="fw-bold">Christmas Party</h3>
+                        <Row lg={2} xs={1}>
                                 <Col className="radius-0 m-auto">
                                     <div className="overflow-hidden p-2 p-lg-4 d-flex justify-content-center">
                                         <Image src={experience} fluid className="zoom" />
                                     </div>
                                 </Col>
                                 <Col className="radius-0 m-auto text-justify ps-lg-0 pe-lg-4">
-                                Indoor plants are ornamental plants that are grown indoors.
-                                They are also known as houseplants, pot plants, or potted plants.
-                                Indoor plants are mainly grown in residences and offices for decorative purposes.
-                                Most houseplants are derived from plants native to the tropics and near tropics.
+                                December 13, 2024 PHLpost Staff celebrating their christmas party held on Himamaylan Beach Resort.
+                                They invited us to join with them as the part of their family.
+                                </Col>
+                            </Row>
+                        </Card>
+                        <Card className="w-50 border-end-2 radius-0 bg-transparent"></Card>
+                        <Card className="w-lg-50 border-0 shadow m-auto p-2 p-lg-3 text-justify position-relative">
+                        <p className="position-absolute top-0 end-0 me-3 mt-1 fw-bold text-primary text-opacity-50">2024</p>
+                        <Card.Title className="text-center fw-bold">Plaza Type</Card.Title>
+                            <Row lg={2} xs={1}>
+                                <Col className="radius-0 m-auto">
+                                    <div className="overflow-hidden p-2 p-lg-4 d-flex justify-content-center">
+                                        <Image src={experience1} fluid className="zoom" />
+                                    </div>
+                                </Col>
+                                <Col className="radius-0 m-auto text-justify ps-lg-0 pe-lg-4">
+                                In this day we doing plaza type distribution of National Identification Card in barangay Oringao, Kabankalan City.
+                                This experience is hard for me because it may challenging to faces any personalities of each people.
                                 </Col>
                             </Row>
                         </Card>
                         <Card className="w-25 m-auto border-x radius-0 bg-transparent"></Card>
                         <Row>
                             <Col>
-                                <Card className="w-lg-75 border-0 shadow ms-auto p-2 p-lg-3 text-justify position-relative">
+                                <Card className="w-lg-70 border-0 shadow ms-auto p-2 p-lg-3 text-justify position-relative">
                                 <p className="position-absolute top-0 end-0 me-3 mt-1 fw-bold text-primary text-opacity-50">2024</p>
-                                <h3 className="fw-bold">title</h3>
-                                Indoor plants are ornamental plants that are grown indoors.
-                                        They are also known as houseplants, pot plants, or potted plants.
-                                        Indoor plants are mainly grown in residences and offices for decorative purposes.
-                                        Most houseplants are derived from plants native to the tropics and near tropics.
+                                <h3 className="fw-bold">Shorting</h3>
+                                <Row lg={2} xs={1}>
+                                <Col className="radius-0 m-auto">
+                                    <div className="overflow-hidden p-2 p-lg-4 d-flex justify-content-center">
+                                        <Image src={experience2} fluid className="zoom" />
+                                    </div>
+                                </Col>
+                                <Col className="radius-0 m-auto text-justify ps-lg-0 pe-lg-4">
+                                In this day, we do a shorting of National Identification ID's. We short per barangay
+                                and after that we short again per sitio that easy for us to search or to find.
+                                </Col>
+                            </Row>
                                 </Card>
                             </Col>
                             <Col>
-                                <Card className="w-lg-75 border-0 shadow me-auto p-2 p-lg-3 text-justify position-relative">
+                                <Card className="w-lg-70 border-0 shadow me-auto p-2 p-lg-3 text-justify position-relative">
                                 <p className="position-absolute top-0 end-0 me-3 mt-1 fw-bold text-primary text-opacity-50">2024</p>
-                                <h3 className="fw-bold">title</h3>
-                                Indoor plants are ornamental plants that are grown indoors.
-                                        They are also known as houseplants, pot plants, or potted plants.
-                                        Indoor plants are mainly grown in residences and offices for decorative purposes.
-                                        Most houseplants are derived from plants native to the tropics and near tropics.
+                                <h3 className="fw-bold">Encoding</h3>
+                                <Row lg={2} xs={1}>
+                                <Col className="radius-0 m-auto">
+                                    <div className="overflow-hidden p-2 p-lg-4 d-flex justify-content-center">
+                                        <Image src={experience3} fluid className="zoom" />
+                                    </div>
+                                </Col>
+                                <Col className="radius-0 m-auto text-justify ps-lg-0 pe-lg-4">
+                                We encode their name and ID's with their serial number that can easy to find in the monitor.
+                                </Col>
+                            </Row>
                                 </Card>
                             </Col>
                         </Row>
